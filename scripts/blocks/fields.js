302545a347288aa3364e8d4ea2f531ffc644a734
lipsum <dayzzzy@gmail.com>
2019-08-04 13:59:31 +0300

    deploy

--- scripts/blocks/fields.js
+++ scripts/blocks/fields.js
@@ -1,7 +1,11 @@
 jQuery(function($){
   var $feildTel = $('input[type="tel"]');
+  var $textareas = $('textarea');
 
   if($feildTel.length) {
     $feildTel.mask("+7 (999) 999-99-99");
   }
+
+  $textareas.html($textareas.html().trim());
+
 });
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,39 @@
+jQuery(document).ready(function($){
+  'use strict';
+	$(function() {
+
+    var anchors = $('.js-anchor');
+    var el;
+    var attr;
+
+    if(anchors.length) {
+      anchors.click(function (){
+        el = $(this);
+        attr = el.attr('href');
+
+        $('html, body').animate({
+          scrollTop: $(attr).offset().top
+        }, 500);
+      });
+    }
+
+  });
+});
+
 jQuery(function($){
   $('.js-fancybox').fancybox();
 });
 
 jQuery(function($){
   var $feildTel = $('input[type="tel"]');
+  var $textareas = $('textarea');
 
   if($feildTel.length) {
     $feildTel.mask("+7 (999) 999-99-99");
   }
+
+  $textareas.html($textareas.html().trim());
+
 });
 
 jQuery(function($){
@@ -91,6 +117,71 @@ jQuery(function($){
   }
 
 });
+
+'use strict';
+
+(function() {
+
+  var modals = document.querySelectorAll('.js-modal');
+
+  console.log('sdfsaafsa')
+
+  if (modals.length) {
+    var btnsClose = document.querySelectorAll('.js-modal-close');
+    var overlays = document.querySelectorAll('.js-modal-overlay');
+
+    var btnsCallback = document.querySelectorAll('.js-callback-button');
+    var callback = document.querySelector('.js-callback-modal');
+    var btnsFeedback = document.querySelectorAll('.js-feedback-button');
+    var feedback = document.querySelector('.js-feedback-modal');
+    var ESC = 27;
+
+    var close = function() {
+      for (var i = 0; i < modals.length; i += 1) {
+        modals[i].classList.remove('active');
+      }
+    }
+
+    for (var i = 0; i < btnsClose.length; i += 1) {
+      btnsClose[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        e.currentTarget.parentNode.parentNode.classList.remove('active');
+      });
+    }
+
+    for (var i = 0; i < overlays.length; i += 1) {
+      overlays[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        e.currentTarget.parentNode.classList.remove('active');
+      });
+    }
+
+    var onEscKeyup = function(e) {
+      if (e.keyCode === ESC) {
+        e.preventDefault();
+        close();
+      }
+    };
+
+    document.addEventListener('keyup', onEscKeyup);
+
+    for (var i = 0; i < btnsCallback.length; i += 1) {
+      btnsCallback[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        callback.classList.add('active');
+      });
+    }
+
+    for (var i = 0; i < btnsFeedback.length; i += 1) {
+      btnsFeedback[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        feedback.classList.add('active');
+      });
+    }
+
+  }
+
+})();
 
 jQuery(function($){
   var objectMap = $('.js-object-map');
